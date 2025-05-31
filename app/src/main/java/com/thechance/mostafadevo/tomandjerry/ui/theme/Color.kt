9d81e975f93c520b176a857f0c53e8dbc10d7c64
Mostafa Mohamed Fahmy <MostafaMohamedFahmy001@gmail.com>
2025-05-31 11:53:00 +0300

    feat: add new vector assets and image
    This commit introduces the following new assets:
    - `chef.xml`: Vector drawable for a chef icon.
    - `heart.xml`: Vector drawable for a heart icon.
    - `ruler.xml`: Vector drawable for a ruler icon.
    - `tomkitckenmeal.png`: PNG image asset.
    
    It also adds new color definitions to `Color.kt`:
    - `black60`
    - `black87`
    - `white87`
    - `white80`

--- app/src/main/java/com/thechance/mostafadevo/tomandjerry/ui/theme/Color.kt
+++ app/src/main/java/com/thechance/mostafadevo/tomandjerry/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val black60 = Color(0x99121212)
+val black87 = Color(0xF71F1F1E)
+
+val white87 = Color(0xDEFFFFFF)
+val white80 = Color(0xCCFFFFFF)
