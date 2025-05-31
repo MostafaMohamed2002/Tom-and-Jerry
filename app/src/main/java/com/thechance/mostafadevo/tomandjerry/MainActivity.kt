90d9e660b5d3d934c687d429bb1f5ffeddb75739
Mostafa Mohamed Fahmy <MostafaMohamedFahmy001@gmail.com>
2025-05-31 14:32:50 +0300

    feat: add MainActivity and set JerryStore as initial screen

---
+++ app/src/main/java/com/thechance/mostafadevo/tomandjerry/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.thechance.mostafadevo.tomandjerry
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.thechance.mostafadevo.tomandjerry.ui.screen.jerryStore.JerryStore
+import com.thechance.mostafadevo.tomandjerry.ui.screen.tomKitcken.TomKitchen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Scaffold(
+            ) {innerPadding->
+                JerryStore(Modifier.padding(innerPadding))
+//                TomKitchen(Modifier.padding(innerPadding))
+            }
+        }
+    }
+}
+
